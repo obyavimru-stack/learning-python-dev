6edc7a9e05c26f0c25ecd2071181cdf88b0937f3
sp79 <sp79@example.com>
2025-11-13 20:36:35 +0300

    Day 1 tasks complete

---
+++ day1/main.py
@@ -0,0 +1,25 @@
+name = "Aleksandr"
+age = 46
+rost = 1.76
+python = True
+print(f"Имя: {name} \nВозраст: {age} \nРост: {rost} \nИзучаю Python: {python}")
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+c = input("Введите операцию:(*)  ")
+c = a*b
+print(f"Результат:{c}")
+
+cel = int(input("Введите температуру в Цельсиях: "))
+far = cel*9/5+32
+print(f"Температура в Фаренгейтах: {far}")
+
+name = input("Введите имя: ")
+age = int(input("Введите возраст: "))
+vosrast = 2025 - age
+print(f"Привет, {name}! Тебе {vosrast} лет.")
+
+a = int(input("Введите число: "))
+print(b = a>0 "Число положительное")
+print(c = a<0 "Число отрицательное")
+print(d = a==0 "Это ноль")
